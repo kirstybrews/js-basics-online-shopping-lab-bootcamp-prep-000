d0278bafbf0b79876a21341c82fc091d50f3e6f5
Kirsty Brewster <kirstybrewster95@gmail.com>
2020-08-17 15:33:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,11 +10,20 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  cart.push( { itemName: item, itemPrice: Math.floor(Math.random() * 100) })
+  return `${item} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  var cartMultipleItems = []
+  if (cart.length > 1) {
+   cartMultipleItems.push(cart.forEach(({itemName, itemPrice}) => `${itemName} at ${itemPrice},`))
+   return "In your cart, you have" + (cartMultipleItems.length - 1).join("") + ", and " + (i => (i + 1) == cart.length).join("") + "."
+  } else if (cart.length == 1) { 
+    cart.forEach(({itemName, itemPrice}) => `In your cart, you have ${itemName} at ${itemPrice}.`)
+  } else {
+    `Your shopping cart is empty.`
+  }
 }
 
 function total() {
